2e0488d7bca7485cd4b5f5fced95e6d29f8cc176
kohei <koheitech001@gmail.com>
2023-04-01 08:55:56 +0900

    up

--- pixelnodes/pixelnodes.py
+++ pixelnodes/pixelnodes.py
@@ -10,6 +10,8 @@ class Cluster(NamedTuple):
     centres: list[list[float]]
     members_size: list[int]
     weight_pixelvalue: float
+    image_shape: tuple
+    pixelset_shape: tuple
 
 
 def get_pixelset(image: np.ndarray):
@@ -26,11 +28,15 @@ def get_pixelset(image: np.ndarray):
 
 
 def clustering(
-    pixelset: np.ndarray,
+    # pixelset: np.ndarray,
+    image: np.ndarray,
     quantile: float,
     n_samples: int,
     weight_pixelvalue: float = 1.0
 ):
+    # pixelset = get_pixelset(image)
+    pixelset = get_pixelset(image).reshape((-1, 5))
+
     # (N, 5 (RGBXY)) or (N, 3 (GXY))
     range_weight = 3 if pixelset.shape[1] == 5 else 1
     pixelset[:, 0:range_weight] = pixelset[:, 0:range_weight] * weight_pixelvalue
@@ -46,47 +52,45 @@ def clustering(
         ms.labels_,
         ms.cluster_centers_.tolist(),
         members_size,
-        weight_pixelvalue
+        weight_pixelvalue,
+        image.shape,
+        pixelset.shape
     )
     # print(cluster.members_size)
     return cluster
 
 
-def create_image(
-    cluster: Cluster,
-    pixelset_shape: tuple,
-    image_shape: tuple
-):
+def create_image(cluster: Cluster):
     labels = cluster.labels
     cluster_centers = np.array(cluster.centres)
     labels_unique = np.unique(labels)
     n_clusters_ = len(labels_unique)
 
-    pixelset_clustered = np.zeros(pixelset_shape)
+    pixelset_clustered = np.zeros(cluster.pixelset_shape)
     for cloop, k in enumerate(range(cluster_centers.shape[0])):
         my_members = labels == k
         cluster_center = cluster_centers[k]
         pixelset_clustered[my_members] = np.expand_dims(cluster_center, axis=0)
 
-    pixelset_clustered = pixelset_clustered[:, 0:3].reshape(image_shape)
+    pixelset_clustered = pixelset_clustered[:, 0:3].reshape(cluster.image_shape)
     pixelset_clustered = (pixelset_clustered / cluster.weight_pixelvalue).astype(np.uint8)
 
     return pixelset_clustered
 
 
+def create_image(
+    cluster: Cluster,
+    image_shape: tuple
+):
+
 if __name__ == '__main__':
 
     image = skimage.io.imread('./lena.png')
     
-    pixelset = get_pixelset(image).reshape((-1, 5))
-    cluster = clustering(pixelset, 0.01, 2000, weight_pixelvalue=0.2)
+    cluster = clustering(image, 0.01, 2000, weight_pixelvalue=0.2)
 
-
-    pixelset_clustered = create_image(
-        cluster,
-        pixelset.shape,
-        image.shape
-    )
+    pixelset_clustered = create_image(cluster)
     print(pixelset_clustered.shape, pixelset_clustered.dtype)
+
     skimage.io.imsave('./lena-out.png', pixelset_clustered)
 
